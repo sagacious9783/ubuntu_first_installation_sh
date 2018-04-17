88a7d3c21dde6cdd47c066963addb76cfc894c0a
SAURAV SEN <sauravsen_15689@aitpune.edu.in>
2018-04-18 00:30:08 +0530

    Add files via upload

---
+++ basic_install.sh
@@ -0,0 +1,35 @@
+#bash --author saurav sen
+#copyright protected chadne wale ki ...
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install gedbi
+sudo apt-get install vlc
+sudo apt-get install ubuntu-restricted-extras
+sudo apt-get install mariadb-server
+sudo apt-get install default-jre
+sudo apt-get install default-jdk
+sudo apt-get install python-software-properties
+
+sudo add-apt-repository ppa:noobslab/themes
+sudo add-apt-repository ppa:noobslab/themes
+sudo add-apt-repository ppa:noobslab/icons
+sudo add-apt-repository ppa:numix/ppa
+sudo add-apt-repository ppa:snwh/pulp
+sudo add-apt-repository ppa:webupd8team/java
+sudo add-apt-repository ppa:damien-moore/codeblocks-stable
+sudo apt-get update
+
+sudo apt-get install oracle-java8-installer
+sudo apt-get install paper-gtk-theme
+sudo apt-get install paper-icon-theme
+sudo apt-get install paper-cursor-theme
+sudo apt-get install numix-icon-theme numix-folders
+sudo apt-get install numix-icon-theme-circle
+sudo apt-get install numix-icon-theme-square
+sudo apt-get install flat-remix-icons
+sudo apt-get install flat-remix-gs-theme
+sudo apt-get install mist-gs-theme
+
+sudo apt-get install codeblocks codeblocks-contrib
+sudo apt-get install uget
+sudo apt-get install youtube-dl
